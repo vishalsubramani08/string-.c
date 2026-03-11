e20109aa9455f95c0a62ce9444d620e48f4d25b4
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-03-11 17:47:14 +0530

    Create str7.c

---
+++ str7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s[50];int count=0;
+    scanf("%s",s);
+    for(int i=0;i<strlen(s);i++){
+        if(s[i]=='a'|| s[i]=='e' || s[i]=='i' || s[i]=='o'|| s[i]=='u' || s[i]=='A'|| s[i]=='E' || s[i]=='I' || s[i]=='O'|| s[i]=='U'){
+            continue;
+        }
+        else{
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
